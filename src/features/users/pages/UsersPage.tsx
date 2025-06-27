24d78523adf9cf0115fd61c36bef2a60f82f2e0b
gasbrieo <gaberabreu.tech@gmail.com>
2025-06-27 13:48:08 -0300

    pog

--- src/features/users/pages/UsersPage.tsx
+++ src/features/users/pages/UsersPage.tsx
@@ -13,8 +13,8 @@ export const UsersPage = () => {
     <div className="flex flex-1 flex-col space-y-4">
       <div className="flex items-start justify-between">
         <div>
-          <h2 className="text-3xl font-bold tracking-tight">Products</h2>
-          <p className="text-muted-foreground text-sm">Manage products (Server side table functionalities.)</p>
+          <h2 className="text-3xl font-bold tracking-tight">Users</h2>
+          <p className="text-muted-foreground text-sm">Manage users (Client side table functionalities.)</p>
         </div>
       </div>
       <Separator />
--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,13 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-const RouteComponent = () => {
-  return (
-    <div className="p-4">
-      <h3>Welcome Home!!!</h3>
-    </div>
-  );
-};
+import { DashboardPage } from "~/features/dashboard";
 
 export const Route = createFileRoute("/dashboard/")({
-  component: RouteComponent,
+  component: DashboardPage,
 });
